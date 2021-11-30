e79ab9a5a328872b61c7e4802a0e96f9a508cd38
Traqq <alexandre.zandecki@yahoo.com>
2021-11-30 19:53:25 +0100

    Photo

--- TriptoEnochillma/Assets/_02Scripts/PhotoSystem.cs
+++ TriptoEnochillma/Assets/_02Scripts/PhotoSystem.cs
@@ -4,38 +4,38 @@ using UnityEngine;
 
 public class PhotoSystem : MonoBehaviour
 {
-
-    [SerializeField] private RenderTexture m_renderTexture;
-    private Vector2Int m_textureSize;
+    [SerializeField] private int resWidth = 2550;
+    [SerializeField] private int resHeight = 3300;
     [SerializeField] private Camera m_camera;
     
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.N))
         {
-            CreateTexture();
+            TakeScreenShot();
         }
     }
 
-    private Texture2D CreateTexture()
+    public string ScreenShotName(int a_width, int a_height)
     {
-        Debug.Log("Smile");
-        //Create 2D texture
-        Texture2D l_texture = new Texture2D(m_textureSize.x, m_textureSize.y, TextureFormat.ARGB32, false);
-
-        // Save the pixel to the render texture
-        m_camera.Render();
-        //Set the render texture to read pixel from
-        RenderTexture.active = m_renderTexture;
-
-        Rect l_targetRect = new Rect(0, 0, m_textureSize.x, m_textureSize.y);
-        l_texture.ReadPixels(l_targetRect, 0, 0);
-        l_texture.Apply();
-
-        RenderTexture.active = null;
-        Debug.Log("PhotoPrinted");
-        return l_texture;
-        
+        return string.Format("{0}/screenshots/screen_{1}x{2}_{3}.png", Application.dataPath, a_width, a_height, System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
     }
+
+    public void TakeScreenShot()
+    {
+        RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
+        m_camera.targetTexture = rt;
+        Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+        m_camera.Render();
+        RenderTexture.active = rt;
+        screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+        m_camera.targetTexture = null;
+        RenderTexture.active = null;
+        Destroy(rt);
+        byte[] bytes = screenShot.EncodeToPNG();
+        string filename = ScreenShotName(resWidth, resHeight);
+        System.IO.File.WriteAllBytes(filename, bytes);
+    }
+
 
 }
