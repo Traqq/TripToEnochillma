0548e6f210d8d6dfa63d0a8329e5f500150afb01
Traqq <alexandre.zandecki@yahoo.com>
2022-01-23 12:01:26 +0100

    dialogue 2

---
+++ TriptoEnochillma/Assets/_02Scripts/AmbianceDialogueEnter.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbianceDialogueEnter : MonoBehaviour
+{
+    [SerializeField] private GameObject ambianceDialogue;
+    [SerializeField] private GameObject cinematicDialogue;
+    [SerializeField] private bool ambianceText;
+    [SerializeField] private bool cinematicText;
+
+    private void Start()
+    {
+        //dialogue.SetActive(false);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            dialogue.SetActive(true);
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            dialogue.SetActive(false);
+        }
+      
+    }
+}
